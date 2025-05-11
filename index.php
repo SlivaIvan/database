f3e4098a1a4846f97d7f372438e35f01001653aa
sliva ivan <ivan.sliva2017@yandex.ru>
2025-05-11 17:42:51 +0300

    initial commit

--- index.php
+++ index.php
@@ -11,14 +11,45 @@ $date_str = "2025-05-21 15:20:00";
 $order_date = date('Y-m-d H:i:s', strtotime($date_str));
 
 
-// Через массив добавляем данные 
-$data = [
+//Примеры создания таблиц
+
+$sql = ' id SERIAL PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    client_id INTEGER REFERENCES client(id),
+    shop_id INTEGER REFERENCES shop(id)';
+
+$db->createTable('product', $sql);
+
+
+// Примеры данных запысываемых в таблицу
+$dataClient = [
+    'name' => 'Петр',
+    'phone' => '89016365452',
+];
+
+$dataOrder = [
+    'shop_id' => 3,
+    'client_id' => 2,
+    'created_at' => $order_date,
+];
+
+$dataOrdedProd = [
     'order_id' => 3,
     'product_id' => 2,
     'quantity' => 1,
     'price_at_order' => 15000,
 ];
 
+$dataProduct = [
+    'name' => 'Пирожки',
+    'price' => 2000,
+    'count' => 1,
+];
+
+$dataShop = [
+    'name' => 'Пятёрочка',
+    'address' => 'Москва, пр-кт Мира д 18',
+];
 // Проверка на POST-запрос
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Вставка в базу данных
@@ -29,6 +60,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 }
 //Метод удаления из таблицы
 //$db->delete($table, 7);
+
+$db->insert('client', $data);
 ?>
 
 <!-- HTML форма для отправки данных -->
